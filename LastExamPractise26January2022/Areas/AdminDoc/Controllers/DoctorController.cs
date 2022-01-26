f09e7b3250cfe5185332b9b2d2ea580161e59344
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-27 00:37:44 +0400

    00:38

---
+++ LastExamPractise26January2022/Areas/AdminDoc/Controllers/DoctorController.cs
@@ -0,0 +1,25 @@
+﻿using LastExamPractise26January2022.Areas.AdminDoc.ViewModels;
+using LastExamPractise26January2022.Data.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LastExamPractise26January2022.Areas.AdminDoc.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private AppDbContext _context { get; }
+        public DoctorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [Area("AdminDoc")]
+        public async Task<IActionResult> Index()
+        {
+            var dbdoctors = await _context.doctors.Where(p => p.IsDeleted == false).ToListAsync();
+          
+            return View(dbdoctors);
+        }
+    }
+}
---
+++ LastExamPractise26January2022/Areas/AdminDoc/ViewModels/DoctorVM.cs
@@ -0,0 +1,11 @@
+﻿namespace LastExamPractise26January2022.Areas.AdminDoc.ViewModels
+{
+    public class DoctorVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+    }
+}
